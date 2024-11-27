b0416e1bc21f964dc537306c0582bd416ff5d9b9
dr1zzleGDShu <c4024637@my.shu.ac.uk>
2024-11-27 00:15:52 +0000

    fixShipMovingOnSpawn

--- evidenceFolder/homework3/solution/t08_spritescrollsol/T09_Scroll/entity.h
+++ evidenceFolder/homework3/solution/t08_spritescrollsol/T09_Scroll/entity.h
@@ -13,10 +13,22 @@
 
 struct entClass {
 	float xPos, yPos;
-	float xVel, yVel;
+	float xVel = 0, yVel = 0;
+    int c = 0; // TODO DEL THIS
     const float maxVel = 20;
 
     sf::Sprite entSpr;
+
+
+    void debugPosOut() {
+        ++c;
+        std::cout << xPos << yPos << ++c;
+    }
+
+    void debugVelOut() {
+        ++c;
+        std::cout << xVel << yVel << ++c;
+    }
 
     void initEnt(sf::Texture& entTexIn, int xPosIn, int yPosIn, float scaleIn) {
         xPos = xPosIn; yPos = yPosIn;
@@ -50,32 +62,31 @@ struct entClass {
         // clamp the value to bounds (converted from ints to floats (bcs of screen size being an int))
         xPos = std::max(static_cast<float>(xBoundMin), std::min(xPos, static_cast<float>(xBoundMax)));
         yPos = std::max(static_cast<float>(yBoundMin), std::min(yPos, static_cast<float>(yBoundMax)));
-
     }
 
     void updateEntPos(int xBoundMinIn, int xBoundMaxIn, int yBoundMinIn, int yBoundMaxIn) {
         // auto update entPos using current velocity
         // assumes velocity is already updated
 
+        // TODO why is yVel so big
         mvEnt(xVel, yVel, xBoundMinIn, xBoundMaxIn, yBoundMinIn, yBoundMaxIn);
-
     }
 };
 
 
 struct entStore {
 
-    std::vector<entClass> entVect = {};
+    std::vector<entClass*> entVect = {};
 
     void drawEntStore(sf::RenderWindow& winIn) {
-        for (entClass i : entVect) {
-            i.renderEnt(winIn);
+        for (entClass* i : entVect) {
+            i->renderEnt(winIn);
         }
     }
 
     void updateEntsPositions(int xBoundMinIn, int xBoundMaxIn, int yBoundMinIn, int yBoundMaxIn) {
-        for (entClass i : entVect) {
-            i.updateEntPos(xBoundMinIn, xBoundMaxIn, yBoundMinIn, yBoundMaxIn);
+        for (entClass* i : entVect) {
+            i->updateEntPos(xBoundMinIn, xBoundMaxIn, yBoundMinIn, yBoundMaxIn);
         }
 
     }
